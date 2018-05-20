ef40532435bc70edb88bc93e3496a74b3da92328
GurlFriday88 <entrotlek@gmail.com>
2018-05-20 18:45:07 -0400

    Initialized project , dl dependencies, and set up local db connection string

---
+++ CapstoneData/CapstoneContext.cs
@@ -0,0 +1,12 @@
+﻿using CapstoneData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CapstoneData
+{
+    public class CapstoneContext : DbContext 
+    {
+        public CapstoneContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Prefix> Prefixes { get; set; }
+    }
+}
---
+++ CapstoneData/Models/Prefix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneData.Models
+{
+    public class Prefix
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        //Foreign Keys
+        //public virtual Provider Provider { get; set; }
+    }
+}
